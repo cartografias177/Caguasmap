f9bb8a29162154553f9876d937f280c7e468a8ae
cartografias177 <cartografias@lamarana.org>
2025-01-30 16:07:19 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/17HFgpsLXL3rFr1U_CJtsGhlXlMuYnNbMXoE7gUB9n_o/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fy-EBXPuT88inwanKWTYzw-SGfAvl1v6/edit?gid=1550956791#gid=1550956791';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
